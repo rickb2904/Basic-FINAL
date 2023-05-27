c882444b01e1f6e8f12857baf06b4540c70aa6ca
rickb <rickos2003@gmail.com>
2023-05-27 12:00:19 +0200

    MAJ

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -19,15 +19,20 @@ class UserController extends AbstractController
      */
     public function index(UserRepository $userRepository): Response
     {
+        // Récupère l'utilisateur actuellement connecté
         $user = $this->getUser();
 
+        // Vérifie le rôle de l'utilisateur et redirige en conséquence
         if ($user !== null && $user->getRole() == "adherent") {
             return $this->redirectToRoute('app_adherent');
         } elseif ($user !== null && $user->getRole() == "coach") {
             return $this->redirectToRoute('app_coach');
         }
+
+        // Récupère tous les utilisateurs depuis le repository
         $users = $userRepository->findAll();
 
+        // Rend la vue 'user/index.html.twig' avec les utilisateurs passés en paramètre
         return $this->render('user/index.html.twig', [
             'users' => $users,
         ]);
@@ -38,39 +43,41 @@ class UserController extends AbstractController
      */
     public function listeUtilisateurs(UserRepository $userRepository): Response
     {
+        // Récupère tous les utilisateurs depuis le repository
         $users = $userRepository->findAll();
 
+        // Rend la vue 'adherent/list.html.twig' avec les utilisateurs passés en paramètre
         return $this->render('adherent/list.html.twig', [
             'users' => $users,
         ]);
     }
-
-
 
     /**
      * @Route("/listusers2", name="app_list_users2")
      */
     public function listeUtilisateurs2(UserRepository $userRepository): Response
     {
+        // Récupère tous les utilisateurs depuis le repository
         $users = $userRepository->findAll();
 
+        // Rend la vue 'coach/list.html.twig' avec les utilisateurs passés en paramètre
         return $this->render('coach/list.html.twig', [
             'users' => $users,
         ]);
     }
-
 
     /**
      * @Route("/adherent", name="app_adherent")
      */
     public function adherent(UserRepository $userRepository, FicheSanteRepository $ficheSanteRepository): Response
     {
+        // Récupère les utilisateurs ayant le rôle "adherent" depuis le repository
         $users = $userRepository->findBy(['role' => 'adherent']);
 
+        // Rend la vue 'adherent/index.html.twig' avec les utilisateurs et les fiches de santé passés en paramètre
         return $this->render('adherent/index.html.twig', [
             'users' => $users,
             'ficheS' => $ficheSanteRepository->findAll(),
-
         ]);
     }
 
@@ -79,22 +86,27 @@ class UserController extends AbstractController
      */
     public function coach(): Response
     {
+        // Récupère le repository des utilisateurs
         $userRepository = $this->getDoctrine()->getRepository(User::class);
+
+        // Récupère les utilisateurs ayant le rôle "adherent" depuis le repository
         $users = $userRepository->findBy(['role' => 'adherent']);
 
+        // Rend la vue 'coach/index.html.twig' avec les utilisateurs passés en paramètre
         return $this->render('coach/index.html.twig', [
             'users' => $users,
         ]);
     }
-
 
     /**
      * @Route("/users/new", name="app_users_new")
      */
     public function new(Request $request): Response
     {
+        // Crée une nouvelle instance de l'entité User
         $user = new User();
 
+        // Vérifie le type de formulaire à créer en fonction de la valeur du paramètre 'type' de la requête
         if ($request->get('type') == 'adherent') {
             $form = $this->createForm(UserTypeAdherent::class, $user);
         } elseif ($request->get('type') == 'coach') {
@@ -103,8 +115,10 @@ class UserController extends AbstractController
             return $this->redirectToRoute('app_users_index');
         }
 
+        // Gère la requête et le formulaire soumis
         $form->handleRequest($request);
 
+        // Vérifie si le formulaire est valide et persiste l'utilisateur en base de données
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($user);
@@ -113,6 +127,7 @@ class UserController extends AbstractController
             return $this->redirectToRoute('app_users_index');
         }
 
+        // Rend le formulaire à afficher dans la vue 'user/new.html.twig'
         return $this->renderForm('user/new.html.twig', [
             'form' => $form,
         ]);
@@ -123,6 +138,7 @@ class UserController extends AbstractController
      */
     public function show(User $user): Response
     {
+        // Vérifie le rôle de l'utilisateur pour choisir la vue appropriée
         if ($user->getRole() == "coach") {
             return $this->render('user/show.html.twig', [
                 'user' => $user,
@@ -134,13 +150,12 @@ class UserController extends AbstractController
         ]);
     }
 
-
     /**
      * @Route("/users/{id}/edit", name="app_user_edit")
      */
     public function edit(Request $request, User $user): Response
-
     {
+        // Vérifie le rôle de l'utilisateur pour choisir le type de formulaire approprié
         if ($user->getRole() == "adherent") {
             $form = $this->createForm(UserTypeAdherent::class, $user);
         } elseif ($user->getRole() == "coach") {
@@ -149,14 +164,17 @@ class UserController extends AbstractController
             return $this->redirectToRoute('app_users_index');
         }
 
+        // Gère la requête et le formulaire soumis
         $form->handleRequest($request);
 
+        // Vérifie si le formulaire est valide et met à jour l'utilisateur en base de données
         if ($form->isSubmitted() && $form->isValid()) {
             $this->getDoctrine()->getManager()->flush();
 
             return $this->redirectToRoute('app_users_index');
         }
 
+        // Vérifie le rôle de l'utilisateur pour choisir la vue appropriée
         if ($user->getRole() == "coach") {
             return $this->render('user/edit.html.twig', [
                 'user' => $user,
@@ -169,11 +187,13 @@ class UserController extends AbstractController
             'form' => $form,
         ]);
     }
+
     /**
      * @Route("/users/{id}/delete", name="user_delete")
      */
     public function delete(Request $request, User $user): Response
     {
+        // Vérifie si le jeton CSRF est valide avant de supprimer l'utilisateur
         if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->request->get('_token'))) {
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->remove($user);
